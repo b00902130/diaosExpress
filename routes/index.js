6e0786c192a5b18d5d12672b0bcd632227ccdcfc
b00902130 <b00902130@ntu.edu.tw>
2014-11-25 20:47:32 +0800

    Add api

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,27 @@ var express = require('express');
 exports.load = function(req, res){
 	res.render('index');
 };
+
+exports.getGoodsByID = function(req, res){
+	res.render('index');
+};
+
+exports.getGoodsByName = function(req, res){
+	res.render('index');
+};
+
+exports.getGoodsListByCategory = function(req, res){
+	res.render('index');
+};
+
+exports.addNewGodds = function(req, res){
+	res.render('index');
+};
+
+exports.deleteGoods = function(req, res){
+	res.render('index');
+};
+
+exports.modifyGoods = function(req, res){
+	res.render('index');
+};
